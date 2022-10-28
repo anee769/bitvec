e2e5f071ca10d27dcfaca02e4a6aafed12c0328f
Manish Meganathan <meganathan.manish@gmail.com>
2022-10-29 05:24:43 +0530

    Refactor BitVec Tests
    - Refactored BitVec test cases to be more unitary and utilize table tests more cleanly with error messages directly in the test case.
    - Significantly reduced the number of test cases without reducing code coverage, this can be beefed up in subsequent iterations

--- bitvec_test.go
+++ bitvec_test.go
@@ -1,10 +1,11 @@
 package bitvec
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBitVec_MaxState(t *testing.T) {
@@ -14,234 +15,290 @@ func TestBitVec_MaxState(t *testing.T) {
 		{1, 1},
 		{2, 3},
 		{4, 15},
+		{8, 255},
 		{64, math.MaxUint64},
 	}
 
-	for tno, test := range tests {
-		vec, _ := NewBitVec(1, test.size)
+	for _, test := range tests {
+		vec, err := NewBitVec(1, test.size)
+		require.Nil(t, err, "Unexpected Error")
 
 		max := vec.MaxState()
-		assert.Equal(t, test.max, max, "Test No. %v", tno)
+		assert.Equal(t, test.max, max)
 	}
-
 }
 
 func TestNewBitVec(t *testing.T) {
 	tests := []struct {
 		count, size uint64
-		output      []uint64
+		data        []uint64
+		err         string
 	}{
-		{20, 70, nil},
-		{20, 8, []uint64{0, 0, 0}},
+		{20, 8, []uint64{0, 0, 0}, ""},
+		{10, 10, []uint64{0, 0}, ""},
+		{12, 24, []uint64{0, 0, 0, 0, 0}, ""},
+		{32, 2, []uint64{0}, ""},
+		{20, 70, nil, "state size greater 64 not allowed"},
 	}
 
 	for _, test := range tests {
 		vec, err := NewBitVec(test.count, test.size)
-		if test.size > MAXVECSIZE {
-			assert.EqualError(t, err, "state Size greater 64 not allowed")
-			assert.Nil(t, vec)
-		} else {
+
+		if test.err == "" {
 			assert.Nil(t, err, "Unexpected Error")
-			assert.Equal(t, test.output, vec.Data)
+			assert.Equal(t, test.data, vec.Data)
+		} else {
+			assert.EqualError(t, err, test.err)
+			assert.Nil(t, vec)
 		}
 	}
-
 }
 
 func TestBitVec_String(t *testing.T) {
 	tests := []struct {
-		count, size uint64
-		data        []uint64
-		output      string
+		bitvec *BitVec
+		output string
 	}{
-		{32, 2, []uint64{3027}, "[32|2] [0000000000000000000000000000000000000000000000000000101111010011]"},
-		{16, 4, []uint64{11297799}, "[16|4] [0000000000000000000000000000000000000000101011000110010000000111]"},
-		{8, 10, []uint64{369099440, 4785074604081152}, "[8|10] [0000000000000000000000000000000000010110000000000000001010110000 0000000000010001000000000000000000000000000000000000000000000000]"},
-		{42, 3, []uint64{81, 9223372036854777604}, "[42|3] [0000000000000000000000000000000000000000000000000000000001010001 1000000000000000000000000000000000000000000000000000011100000100]"},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{3027}},
+			"[32|2] [0000000000000000000000000000000000000000000000000000101111010011]",
+		},
+		{
+			&BitVec{Count: 16, Size: 4, Data: []uint64{11297799}},
+			"[16|4] [0000000000000000000000000000000000000000101011000110010000000111]",
+		},
+		{
+			&BitVec{Count: 8, Size: 10, Data: []uint64{369099440, 4785074604081152}},
+			"[8|10] [0000000000000000000000000000000000010110000000000000001010110000 0000000000010001000000000000000000000000000000000000000000000000]",
+		},
+		{
+			&BitVec{Count: 42, Size: 3, Data: []uint64{81, 9223372036854777604}},
+			"[42|3] [0000000000000000000000000000000000000000000000000000000001010001 1000000000000000000000000000000000000000000000000000011100000100]",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
-
-		vec.Data = test.data
-		assert.Equal(t, test.output, fmt.Sprint(vec))
+		assert.Equal(t, test.output, test.bitvec.String())
 	}
 }
 
 func TestBitVec_Set(t *testing.T) {
 	tests := []struct {
-		count, size          uint64
-		index, state, output []uint64
+		bitvec   *BitVec
+		idx, val uint64
+		output   []uint64
+		err      string
 	}{
-		{32, 2, []uint64{30, 31, 27, 28, 26, 29}, []uint64{0, 3, 3, 3, 2, 1}, []uint64{3027}},
-		{16, 4, []uint64{10, 11, 15, 12, 13, 14}, []uint64{10, 12, 7, 6, 4, 0}, []uint64{11297799}},
-		{0, 0, []uint64{}, []uint64{}, []uint64{}},
-		{20, 64, []uint64{6, 4, 3, 1, 0, 10, 12, 16}, []uint64{111, 1000, 36510, 12000, 4, 786, 1001, 9999}, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0, 786, 0, 1001, 0, 0, 0, 9999, 0, 0, 0}},
-		{16, 32, []uint64{1, 9, 5, 13, 7}, []uint64{33, 5676, 12101, 333, 1001}, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}},
-		{8, 10, []uint64{5, 7, 3}, []uint64{43, 17, 22}, []uint64{369099440, 4785074604081152}},
-		{42, 3, []uint64{19, 21, 41, 39}, []uint64{5, 6, 1, 7}, []uint64{81, 9223372036854777604}},
-		{30, 3, []uint64{31}, []uint64{5}, []uint64{0, 0}},
-		{42, 3, []uint64{19, 21, 41, 39, 0}, []uint64{5, 6, 1, 7, 8}, []uint64{81, 9223372036854777604}},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{0}},
+			30, 3, []uint64{12}, "",
+		},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{12}},
+			10, 2, []uint64{8796093022220}, "",
+		},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{0}},
+			14, 0, []uint64{0}, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{0, 0}},
+			3, 3, []uint64{12884901888, 0}, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 0}},
+			10, 2, []uint64{12884901888, 2199023255552}, "",
+		},
+		{
+			&BitVec{Count: 42, Size: 3, Data: []uint64{0, 0}},
+			21, 5, []uint64{1, 4611686018427387904}, "",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			30, 12, []uint64{0}, "index too large for bitvec count (max: 10)",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			9, 18, []uint64{0}, "state too large for bitvec state (max: 15)",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
+		err := test.bitvec.Set(test.idx, test.val)
+		assert.Equal(t, test.bitvec.Data, test.output)
 
-		for i := 0; i < len(test.index); i++ {
-
-			err := vec.Set(test.index[i], test.state[i])
-			if err != nil {
-				if test.index[i] >= test.count {
-					assert.EqualError(t, err, fmt.Sprintf("index too large for BitVec Count (max: %v)", test.count))
-				} else if test.state[i] > vec.MaxState() {
-					assert.EqualError(t, err, fmt.Sprintf("state too large for BitVec state (maxL %v)", vec.MaxState()))
-				} else {
-					assert.Nil(t, err, "Unexpected Error")
-				}
-			}
-
+		if test.err == "" {
+			assert.Nil(t, err, "Unexpected Error")
+		} else {
+			assert.EqualError(t, err, test.err)
 		}
-
-		assert.Equal(t, test.output, vec.Data)
 	}
-
 }
 
 func TestBitVec_Unset(t *testing.T) {
 	tests := []struct {
-		count, size, index uint64
-		data, output       []uint64
+		bitvec *BitVec
+		idx    uint64
+		output []uint64
+		err    string
 	}{
-		{32, 2, 28, []uint64{3027}, []uint64{2835}},
-		{16, 4, 11, []uint64{11297799}, []uint64{10511367}},
-		{20, 64, 16, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0, 786, 0, 1001, 0, 0, 0, 9999, 0, 0, 0}, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0, 786, 0, 1001, 0, 0, 0, 0, 0, 0, 0}},
-		{16, 32, 8, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}},
-		{16, 32, 13, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, []uint64{33, 0, 12101, 1001, 5676, 0, 0, 0}},
-		{8, 10, 7, []uint64{369099440, 4785074604081152}, []uint64{369099440, 0}},
-		{16, 4, 7, []uint64{11297799}, []uint64{11297799}},
-		{42, 3, 21, []uint64{81, 9223372036854777604}, []uint64{80, 1796}},
-		{42, 3, 49, []uint64{81, 9223372036854777604}, []uint64{81, 9223372036854777604}},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{12}},
+			30, []uint64{0}, "",
+		},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{8796093022220}},
+			10, []uint64{12}, "",
+		},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{0}},
+			14, []uint64{0}, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 0}},
+			3, []uint64{0, 0}, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 2199023255552}},
+			10, []uint64{12884901888, 0}, "",
+		},
+		{
+			&BitVec{Count: 42, Size: 3, Data: []uint64{1, 4611686018427387904}},
+			21, []uint64{0, 0}, "",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			30, []uint64{0}, "index too large for bitvec count (max: 10)",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
+		err := test.bitvec.Unset(test.idx)
+		assert.Equal(t, test.bitvec.Data, test.output)
 
-		vec.Data = test.data
-		err = vec.Unset(test.index)
-		if err != nil {
-			if test.index >= test.count {
-				assert.EqualError(t, err, fmt.Sprintf("index too large for BitVec Count (max: %v)", test.count))
-			} else {
-				assert.Nil(t, err, "Unexpected Error")
-			}
+		if test.err == "" {
+			assert.Nil(t, err, "Unexpected Error")
+		} else {
+			assert.EqualError(t, err, test.err)
 		}
-		assert.Equal(t, test.output, vec.Data)
 	}
 }
 
 func TestBitVec_Has(t *testing.T) {
 	tests := []struct {
-		count, size, index, state uint64
-		data                      []uint64
-		output                    bool
+		bitvec   *BitVec
+		idx, val uint64
+		exists   bool
+		err      string
 	}{
-		{32, 2, 27, 3, []uint64{3027}, true},
-		{16, 4, 15, 7, []uint64{11297799}, true},
-		{16, 4, 12, 8, []uint64{11297799}, false},
-		{20, 64, 11, 0, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0, 786, 0, 1001, 0, 0, 0, 9999, 0, 0, 0}, true},
-		{16, 32, 13, 333, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, true},
-		{16, 32, 0, 137, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, false},
-		{8, 10, 4, 199, []uint64{369099440, 4785074604081152}, false},
-		{8, 10, 7, 17, []uint64{369099440, 4785074604081152}, true},
-		{42, 3, 21, 6, []uint64{81, 9223372036854777604}, true},
-		{42, 3, 16, 3, []uint64{81, 9223372036854777604}, false},
-		{42, 3, 50, 3, []uint64{81, 9223372036854777604}, false},
-		{8, 10, 7, 10000, []uint64{369099440, 4785074604081152}, false},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{12}},
+			30, 3, true, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 0}},
+			3, 0, false, "",
+		},
+		{
+			&BitVec{Count: 42, Size: 3, Data: []uint64{1, 4611686018427387904}},
+			21, 0, false, "",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			30, 12, false, "index too large for bitvec count (max: 10)",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			9, 18, false, "state too large for bitvec state (max: 15)",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
+		exists, err := test.bitvec.Has(test.idx, test.val)
+		assert.Equal(t, test.exists, exists)
 
-		vec.Data = test.data
-		check, err2 := vec.Has(test.index, test.state)
-		if err2 != nil {
-			if test.index >= test.count {
-				assert.EqualError(t, err2, fmt.Sprintf("index too large for BitVec Count (max: %v)", test.count))
-			} else if test.state > vec.MaxState() {
-				assert.EqualError(t, err2, fmt.Sprintf("state too large for BitVec state (maxL %v)", vec.MaxState()))
-			} else {
-				assert.Nil(t, err2, "Unexpected Error")
-			}
+		if test.err == "" {
+			assert.Nil(t, err, "Unexpected Error")
+		} else {
+			assert.EqualError(t, err, test.err)
 		}
-		assert.Equal(t, test.output, check)
 	}
 }
 
 func TestBitVec_State(t *testing.T) {
 	tests := []struct {
-		count, size, index uint64
-		data               []uint64
-		output             uint64
+		bitvec      *BitVec
+		idx, output uint64
+		err         string
 	}{
-		{32, 2, 27, []uint64{3027}, 3},
-		{16, 4, 15, []uint64{11297799}, 7},
-		{16, 4, 12, []uint64{11297799}, 6},
-		{20, 64, 11, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0, 786, 0, 1001, 0, 0, 0, 9999, 0, 0, 0}, 0},
-		{16, 32, 13, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, 333},
-		{16, 32, 0, []uint64{33, 0, 12101, 1001, 5676, 0, 333, 0}, 0},
-		{8, 10, 4, []uint64{369099440, 4785074604081152}, 0},
-		{8, 10, 7, []uint64{369099440, 4785074604081152}, 17},
-		{42, 3, 21, []uint64{81, 9223372036854777604}, 6},
-		{42, 3, 10, []uint64{81, 9223372036854777604}, 0},
-		{42, 3, 50, []uint64{81, 9223372036854777604}, 0},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{12}},
+			30, 3, "",
+		},
+		{
+			&BitVec{Count: 32, Size: 2, Data: []uint64{12}},
+			10, 0, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 0}},
+			3, 3, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 0}},
+			5, 0, "",
+		},
+		{
+			&BitVec{Count: 42, Size: 3, Data: []uint64{1, 4611686018427387904}},
+			21, 5, "",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			30, 0, "index too large for bitvec count (max: 10)",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
+		state, err := test.bitvec.State(test.idx)
+		assert.Equal(t, test.output, state)
 
-		vec.Data = test.data
-		value, err2 := vec.State(test.index)
-		if err2 != nil {
-			if test.index >= test.count {
-				assert.EqualError(t, err2, fmt.Sprintf("index too large for BitVec Count (max: %v)", test.count))
-			} else {
-				assert.Nil(t, err2, "Unexpected Error")
-			}
+		if test.err == "" {
+			assert.Nil(t, err, "Unexpected Error")
+		} else {
+			assert.EqualError(t, err, test.err)
 		}
-		assert.Equal(t, test.output, value)
 	}
 }
 
-func TestBitVec_GetIndexes(t *testing.T) {
+func TestBitVec_Indexes(t *testing.T) {
 	tests := []struct {
-		count, size, state uint64
-		data, output       []uint64
+		bitvec  *BitVec
+		query   uint64
+		indexes []uint64
+		err     string
 	}{
-		{32, 2, 3, []uint64{3027}, []uint64{27, 28, 31}},
-		{16, 4, 1, []uint64{11297799}, []uint64{}},
-		{10, 64, 0, []uint64{4, 12000, 0, 36510, 1000, 0, 111, 0, 0, 0}, []uint64{2, 5, 7, 8, 9}},
-		{8, 10, 17, []uint64{369099440, 4785074604081152}, []uint64{7}},
-		{42, 3, 21, []uint64{81, 9223372036854777604}, nil},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 12884901888}},
+			5, []uint64{}, "",
+		},
+		{
+			&BitVec{Count: 16, Size: 8, Data: []uint64{12884901888, 12884901888}},
+			3, []uint64{3, 11}, "",
+		},
+		{
+			&BitVec{Count: 10, Size: 4, Data: []uint64{0}},
+			18, []uint64{}, "state too large for bitvec state (max: 15)",
+		},
 	}
 
 	for _, test := range tests {
-		vec, err := NewBitVec(test.count, test.size)
-		assert.Nil(t, err, "Unexpected Error")
+		indexes, err := test.bitvec.Indexes(test.query)
 
-		vec.Data = test.data
-		indexes, err2 := vec.GetIndexes(test.state)
-		if err2 != nil {
-			if test.state > vec.MaxState() {
-				assert.EqualError(t, err2, fmt.Sprintf("state too large for BitVec state (maxL %v)", vec.MaxState()))
-			} else {
-				assert.Nil(t, err2, "Unexpected Error")
-			}
+		if test.err == "" {
+			assert.Nil(t, err, "Unexpected Error")
+			assert.Equal(t, test.indexes, indexes)
+		} else {
+			assert.EqualError(t, err, test.err)
+			assert.Nil(t, indexes)
 		}
-		assert.Equal(t, test.output, indexes)
 	}
 }
